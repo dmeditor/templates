0fc6cb1137ff17c17608cd574b1a419c9404168b
Chen Xiongjie <chen.xcmail@gmail.com>
2023-04-30 13:31:40 +0200

    Update templates.js

--- templates.js
+++ templates.js
@@ -1,1 +1,18 @@
-
+var dmeditor = {
+templates:[
+  {
+          blocktype: 'heading',        
+          identifier:'sample', 
+          name:'Block heading', 
+          css:`background:#ffcc00; 
+          h2{
+              text-align:center;
+          }`,
+          initData: ()=>{
+            const data = {type:'heading', settings:{level: 2}};
+            return {...data, data:'Hello1', common:{...data.common, color: '#9C27B0' }}
+          }, 
+      }
+]
+}
+        
