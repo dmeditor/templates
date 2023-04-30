78960aea599c3a9666be0cb7666059e234ba8ce6
Chen Xiongjie <chen.xcmail@gmail.com>
2023-04-30 14:09:07 +0200

    Update templates.js

--- templates.js
+++ templates.js
@@ -10,11 +10,10 @@ templates:[
             letter-spacing: 2px;
           h1, h2, h3, h4, h5{
               text-align:center;
-              color: white;
           }`,
           initData: ()=>{
             const data = {type:'heading', settings:{level: 2}};
-            return {...data, data:'Hello1', common:{...data.common, color: '#9C27B0' }}
+            return {...data, data:'Hello1', common:{...data.common, color: '#ffffff' }}
           }, 
       }
 ]
