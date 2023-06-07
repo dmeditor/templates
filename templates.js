d965c28a888539105fbe91c0400c6584ba87d6ca
Chen Xiongjie <chen.xcmail@gmail.com>
2023-06-07 10:36:08 +0200

    Update templates.js

--- templates.js
+++ templates.js
@@ -7,8 +7,9 @@ styles:[
           css:`
             background-image: linear-gradient(45deg, #7242f7, #ec2eaa);
             padding: 10px;
+            color: white;
             letter-spacing: 2px;
-          h1, h2, h3, h4, h5{
+            h1, h2, h3, h4, h5{
               text-align:center;
           }`         
       }
