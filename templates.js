c613b686fccab8304818f645999c5751a9126b13
Chen Xiongjie <chen.xcmail@gmail.com>
2023-06-20 11:44:46 +0200

    Update templates.js

--- templates.js
+++ templates.js
@@ -12,7 +12,30 @@ styles:[
             h1, h2, h3, h4, h5{
               text-align:center;
           }`         
-      }
+      },
+  {
+        blocktype: 'collapsable_text',
+        identifier:'button_center',
+        name:'Center button',
+        css:`
+            margin-bottom: 15px;
+            .dme-common-title{
+              text-align: center;
+              font-weight: bold;
+              font-size: 1.1rem;
+            }
+
+            .dme-common-title span{
+              display:inline-block;
+              padding: 5px 10px;
+              color: #dd4815;
+  border: 1px solid #dd4815;
+            }
+.dme-common-title span::after{
+  color: #dd4815
+}
+        `
+    }
 ]
 }
         
